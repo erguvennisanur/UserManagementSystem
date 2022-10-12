03dbc83cfde7d41fa2c5c3e2b216546ea462145a
erguvennisanur <86314179+erguvennisanur@users.noreply.github.com>
2022-10-13 00:15:53 +0300

    Add files via upload

---
+++ UserManagementCommon/User.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace UserManagementCommon
+{
+    public class User
+    {
+
+        [Key]
+        public int Id 
+        {   get;
+            set; 
+        } 
+
+       
+        public string firstName
+        {
+            get;
+            set;
+        } = String.Empty;
+
+        public string lastName
+        {
+
+            get;
+            set;
+        } = String.Empty;
+
+        public bool activepassive
+        {
+            get;
+            set;
+        } = false;
+        public string email
+        {
+
+            get;
+            set;
+        } = String.Empty;
+        public string address
+        {
+
+            get;
+            set;
+        } = String.Empty;
+        public string telNo
+        {
+            get;
+            set;
+        } = String.Empty;
+
+        public DateTime? Date
+        {
+            get; 
+            set;
+        } = DateTime.Now;
+    }
+}
